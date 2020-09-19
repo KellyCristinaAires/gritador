cd5f5037a492e8c69a1dc65a28dd4bb940025a9b
KellyCristinaAires <k.woys@hotmail.com>
2020-09-18 22:42:40 -0300

    atividade gritador

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string refrigerante;
+            Console.Write("Digite refrigerante: ");
+            refrigerante = Console.ReadLine();
+            Console.Write("\n");
+            Console.WriteLine($"{refrigerante}!!!".ToUpper());
+            Console.Write("\n");
+            Console.WriteLine("Precione enter para sair");
+            Console.ReadKey();
+        }
+    }
+}
